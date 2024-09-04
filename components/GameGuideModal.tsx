b758f606b6bf16d57cc6a85243a0c31c073f54ec
trid3r <alex_yhd@hotmail.com>
2024-09-03 18:43:57 -0600

    Feat: Add dynamic marker fading, optimize AI, and upgrade visual animations and styles.

--- components/GameGuideModal.tsx
+++ components/GameGuideModal.tsx
@@ -9,45 +9,36 @@ export function GameGuideModal({
   return (
     <div class={`fixed inset-0 flex items-center justify-center ${darkMode ? 'bg-gray-800 bg-opacity-75' : 'bg-gray-200 bg-opacity-75'}`}>
       <div class={`rounded-lg shadow-lg p-6 max-w-md w-full ${darkMode ? 'bg-gray-900 text-white' : 'bg-white text-black'}`}>
-        <h2 class="text-2xl font-bold mb-4">Game Guide</h2>
+        <h2 class="text-3xl font-bold mb-4 flex justify-center">Game Guide</h2>
         <p class="mb-4">
-          <strong>Objective:</strong> The goal of the game is to align three of your symbols (either 'X' or 'O') in a row, either horizontally, vertically, or diagonally.
+          <strong>How to Play:</strong> Come on, it's just a Tic Tac Toe but with lives and your marks disappear after some time.
         </p>
         <p class="mb-4">
-          <strong>How to Play:</strong> You are playing against the computer. Click on any cell to place your symbol ('X'). The computer will then make its move with 'O'.
+          <strong>Objective:</strong> Resist as long as possible while the reaction time of the machine increases until you lose your 5 lives.
         </p>
         <p class="mb-4">
-          <strong>Winning the Game:</strong> Align three of your symbols in a row to win. If all cells are filled and no one has three in a row, the game is a tie.
+          <strong>Winning the Game:</strong> You can't win ¯\_(ツ)_/¯ but you can have the best record.
         </p>
         <p class="mb-4">
-          <strong>Game Over:</strong> The game ends when either you or the computer wins, or all cells are filled resulting in a tie. If you lose, you will lose a life.
+          <strong>Game Over:</strong> Ends when you lose all your lifes.
         </p>
         <div class="mb-4 flex justify-center">
+          <p class="mt-1">All the web...</p>
           <a href="https://fresh.deno.dev">
-            {darkMode ? (
-              <img
-                width="197"
-                height="37"
-                src="https://fresh.deno.dev/fresh-badge-dark.svg"
-                alt="Made with Fresh"
-                class="animate-fade-in"
-              />
-            ) : (
-              <img
-                width="197"
-                height="37"
-                src="https://fresh.deno.dev/fresh-badge.svg"
-                alt="Made with Fresh"
-                class="animate-fade-in"
-              />
-            )}
+            <img
+              width="197"
+              height="37"
+              src={`https://fresh.deno.dev/fresh-badge${darkMode ? '-dark' : ''}.svg`}
+              alt="Made with Fresh"
+              class="animate-fade-in"
+            />
           </a>
         </div>
         <button
           class={`w-full text-white px-4 py-2 rounded-lg transition duration-300 ${darkMode ? 'bg-blue-600 hover:bg-blue-700' : 'bg-blue-500 hover:bg-blue-600'}`}
           onClick={onClose}
         >
-          Close
+           <strong><i>Good Game</i></strong>
         </button>
       </div>
     </div>
--- islands/GameBoard.tsx
+++ islands/GameBoard.tsx
@@ -1,42 +1,74 @@
-import { useState } from "preact/hooks";
+import { useState, useEffect } from "preact/hooks";
 import { Header } from "$components/Header.tsx";
 import { Footer } from "$components/Footer.tsx";
 import { GameOverModal } from "$components/GameOverModal.tsx";
 
+interface CellData {
+  value: string | null;
+  timer: number | null;
+  fadeOut: boolean;
+}
+
 export function GameBoard() {
   const [gameStarted, setGameStarted] = useState(false);
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [board, setBoard] = useState<CellData[]>(Array(9).fill({ value: null, timer: null, fadeOut: false }));
   const [playerTurn, setPlayerTurn] = useState(true);
-  const [playerLives, setPlayerLives] = useState(3);
+  const [playerLives, setPlayerLives] = useState(10);
   const [playerPieces, setPlayerPieces] = useState<number[]>([]);
   const [computerPieces, setComputerPieces] = useState<number[]>([]);
   const [gameOver, setGameOver] = useState(false);
   const [showGameOverModal, setShowGameOverModal] = useState(false);
   const [darkMode, setDarkMode] = useState(true);
+  const [boardStatus, setBoardStatus] = useState(4); // 1: Win / 2: Tie / 3: Lose / 4: Normal
   const [boardAnimation, setBoardAnimation] = useState("");
   const [totalPlayerMarks, setTotalPlayerMarks] = useState(0);
   const [totalMatches, setTotalMatches] = useState(0);
   const [computerDelay, setComputerDelay] = useState(500);
 
-   // Toggles dark mode
-   const toggleDarkMode = () => {
+  // Loop to check mark's timer and update it
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setBoard((prevBoard) => {
+        return prevBoard.map((cell) => {
+          if(cell == null) return {value: null, timer: null, fadeOut: false };
+          if (cell.timer === null) return cell;
+          
+          const newTimer = cell.timer - 0.05; // Reduce 0.05 seconds per loop
+          
+          if (newTimer > 0) {
+            return { ...cell, timer: newTimer };
+          } else if (!cell.fadeOut) {
+            return { ...cell, timer: 0, fadeOut: true };
+          } else {
+            return { value: null, timer: null, fadeOut: false };
+          }
+        });
+      });
+    }, 50); // Actualizamos cada 50ms para un control más preciso
+
+    return () => clearInterval(interval);
+  }, []);
+
+  // Toggles dark mode
+  const toggleDarkMode = () => {
     setDarkMode(!darkMode);
   };
 
   // Checks if the game board is full without any winners (tie)
-  const checkTie = (board: (string | null)[]) => {
-    return board.every((cell) => cell !== null);
+  const checkTie = (board: (CellData | null)[]) => {
+    return board.every((cell) => cell !== null && cell.value !== null);
   };
 
   // Handles a player's move on the game board
   const handleCellClick = (index: number) => {
+    // Check if it is the start of the game or return if the game is over or is the computer's turn
     if (!gameStarted) {
       setGameStarted(true);
     }
-    if (board[index] || !playerTurn || gameOver) return;
+    if (board[index].value || !playerTurn || gameOver) return;
 
     const newBoard = [...board];
-    newBoard[index] = "X";
+    newBoard[index] = { value: "X", timer: 3.0, fadeOut: false };
     setBoard(newBoard);
     setPlayerTurn(false);
     setPlayerPieces([...playerPieces, index]);
@@ -49,74 +81,82 @@ export function GameBoard() {
 
     setTimeout(() => {
       computerMove(newBoard);
-    }, computerDelay); 
+    }, computerDelay);
   };
 
-  // Minimax algorithm to determine the best move for the computer
-  const minimax = (board: (string | null)[], isMaximizing: boolean): number => {
-    const scores: { [key: string]: number } = {
-        'X': -10,
-        'O': 10,
-        'tie': 0
-    };
-
-    const winner = (player: string) => checkWin(board, player);
-    
-    if (winner('O')) return scores['O'];
-    if (winner('X')) return scores['X'];
-    if (checkTie(board)) return scores['tie'];
-
-    const emptyIndices = board.reduce(
-        (acc, cell, idx) => (cell === null ? [...acc, idx] : acc),
-        [] as number[]
-    );
-
-    if (emptyIndices.length === 0) return 0;
-
-    let bestScore = isMaximizing ? -Infinity : Infinity;
-
-    for (const index of emptyIndices) {
-        board[index] = isMaximizing ? 'O' : 'X';
-        const score = minimax(board, !isMaximizing);
-        board[index] = null;
-
-        if (isMaximizing) {
-            bestScore = Math.max(score, bestScore);
-        } else {
-            bestScore = Math.min(score, bestScore);
-        }
-    }
-
-    return bestScore;
+  // Helper function to check if a move would result in a win
+  const wouldWin = (board: (CellData | null)[], player: string, index: number) => {
+    const newBoard = [...board];
+    newBoard[index] = { value: player, timer: null, fadeOut: false };
+    return checkWin(newBoard, player);
   };
 
-  // Determines the computer's move and updates the board
-  const computerMove = (currentBoard: (string | null)[]) => {
-    const emptyIndices = currentBoard.reduce(
-      (acc, cell, idx) => (cell === null ? [...acc, idx] : acc),
-      [] as number[]
-    );
-
-    let computerMove = -1;
-    let bestScore = -Infinity;
-
-    for (const index of emptyIndices) {
-        currentBoard[index] = 'O';
-        const score = minimax(currentBoard, false);
-        currentBoard[index] = null;
-
-        if (score > bestScore) {
-            bestScore = score;
-            computerMove = index;
+  // Determines the computer's move
+  const getBestMove = (currentBoard: (CellData | null)[]): number => {
+    // Check if computer can win in the next move
+    for (let i = 0; i < currentBoard.length; i++) {
+      if (!currentBoard[i] || currentBoard[i]?.value === null) {
+        if (wouldWin(currentBoard, "O", i)) {
+          return i;
         }
+      }
     }
   
-    const newBoard = [...currentBoard];
-    newBoard[computerMove] = "O";
-    setBoard(newBoard);
+    // Block player's winning move
+    for (let i = 0; i < currentBoard.length; i++) {
+      if (!currentBoard[i] || currentBoard[i]?.value === null) {
+        if (wouldWin(currentBoard, "X", i)) {
+          return i;
+        }
+      }
+    }
+  
+    // Try to take the center if it's free
+    if (!currentBoard[4] || currentBoard[4]?.value === null) {
+      return 4;
+    }
+  
+    // Try to take the corners
+    const corners = [0, 2, 6, 8];
+    const freeCorners = corners.filter(i => !currentBoard[i] || currentBoard[i]?.value === null);
+    if (freeCorners.length > 0) {
+      return freeCorners[Math.floor(Math.random() * freeCorners.length)];
+    }
+  
+    // Take any available edge
+    const edges = [1, 3, 5, 7];
+    const freeEdges = edges.filter(i => !currentBoard[i] || currentBoard[i]?.value === null);
+    if (freeEdges.length > 0) {
+      return freeEdges[Math.floor(Math.random() * freeEdges.length)];
+    }
+  
+    // If we get here, there are no moves left (shouldn't happen in a normal game)
+    console.error("No valid moves left");
+    return -1; // Invalid move
+  };
+
+  // Make the computer move in the board
+  const computerMove = (currentBoard: CellData[]) => {
+    const computerIndex = getBestMove(currentBoard);
+
+    // Check if is a invalid match or just a tie
+    if (computerIndex === -1) {
+      if (checkTie(currentBoard as CellData[])) {
+        tieTrigger();
+        resetMatch();
+      } else {
+        console.error("Computer couldn't make a move");
+        return;
+      }
+    }
+
+    // Define the computer mark in the correct flow
+    const newBoard = currentBoard.map(cell => cell ? { ...cell } : null);
+    newBoard[computerIndex] = { value: "O", timer: 3.0, fadeOut: false };
+    setBoard(newBoard as CellData[]);
     setPlayerTurn(true);
-    setComputerPieces([...computerPieces, computerMove]);
-  
+    setComputerPieces([...computerPieces, computerIndex]);
+
     if (checkWin(newBoard, "O")) {
       setPlayerLives((lives) => lives - 1);
       loseTrigger();
@@ -127,14 +167,14 @@ export function GameBoard() {
       } else {
         resetMatch();
       }
-    } else if (checkTie(newBoard)) {
+    } else if (checkTie(newBoard as CellData[])) {
       tieTrigger();
       resetMatch();
     }
   };
 
   // Checks if a player has won the game
-  const checkWin = (board: (string | null)[], player: string) => {
+  const checkWin = (board: (CellData | null)[], player: string) => {
     const winningCombos = [
       [0, 1, 2],
       [3, 4, 5],
@@ -145,26 +185,29 @@ export function GameBoard() {
       [0, 4, 8],
       [2, 4, 6],
     ];
-
+  
     return winningCombos.some((combo) =>
-      combo.every((index) => board[index] === player)
+      combo.every((index) => board[index]?.value === player)
     );
   };
 
   // Triggers an animation for a tie
   const tieTrigger = () => {
+    setBoardStatus(2);
     setBoardAnimation("rotation-animation"); 
     setTimeout(() => setBoardAnimation(""), 500);
   };
 
   // Triggers an animation for a loss
   const loseTrigger = () => {
+    setBoardStatus(3);
     setBoardAnimation("shake-animation"); 
     setTimeout(() => setBoardAnimation(""), 500);
   };
 
   // Triggers an animation for a win
   const winTrigger = () => {
+    setBoardStatus(1);
     setBoardAnimation("animate-fade-in"); 
     setTimeout(() => setBoardAnimation(""), 500); 
     resetMatch();
@@ -180,14 +223,15 @@ export function GameBoard() {
 
   // Resets the game state for a new match
   const resetMatch = () => {
-    setBoard(Array(9).fill(null));
+    setBoard(Array(9).fill({ value: null, timer: null }));
     setPlayerPieces([]);
     setComputerPieces([]);
     setTotalMatches((prev) => prev + 1);
     setPlayerTurn(false);
     setComputerDelay((prevDelay) => Math.max(prevDelay - 50, 0));
+    setTimeout(() => setBoardStatus(4), 500); // Delay to show the status with the edge color
 
-    setTimeout(() => {
+    setTimeout(() => { 
       computerMove(Array(9).fill(null));
     }, computerDelay);
   };
@@ -207,17 +251,22 @@ export function GameBoard() {
           {board.map((cell, index) => (
             <button
               key={index}
-              class={`w-full h-full flex items-center justify-center pt-0 text-6xl lg:text-9xl font-bold transition-all duration-300 ${
-                darkMode ? 'bg-gray-700 hover:bg-gray-500' : 'bg-gray-200 hover:bg-gray-400'}`}
+              class={`w-full h-full flex items-center justify-center pt-0 text-6xl lg:text-9xl font-bold transition-all duration-300 ring-2 
+                ${ darkMode ? 'bg-gray-700 hover:bg-gray-500' : 'bg-gray-200 hover:bg-gray-400'}
+                ${ boardStatus == 3 ? 'ring-red-900' : boardStatus == 2 ? 'ring-white' : boardStatus == 1 ? 'ring-green-800' : 'ring-transparent'}`}
               onClick={() => handleCellClick(index)}
             >
-              {cell && (
+              {cell && cell.value && (
                 <span
                   class={`${
-                    cell === "X" ? "text-red-500" : "text-blue-500"
+                    cell.value === "X" ? "text-red-500" : "text-blue-500"
                   } animate-fade-in absolute`}
+                  style={{
+                    opacity: cell.timer ? cell.timer / 3 : cell.fadeOut ? 0 : 1,
+                    transition: "opacity 0.05s linear"
+                  }}
                 >
-                  {cell}
+                  {cell.value}
                 </span>
               )}
             </button>
@@ -228,7 +277,7 @@ export function GameBoard() {
       {gameStarted && (
         <>
           <div class="fixed bottom-0 left-0 right-0 p-2 sm:p-4 bg-gray-800 text-white text-center text-sm sm:text-base animate-slide-up">
-            {playerTurn ? "Your turn" : "Computer's turn"}
+            {playerTurn ? "Your turn" : "Chappie's turn"}
           </div>
           <div class="fixed top-0 left-0 right-0 flex justify-between p-2 sm:p-4 bg-gray-800 text-white text-sm sm:text-base animate-slide-down">
             <div>You: <b>{playerLives}</b> ❤️</div>
